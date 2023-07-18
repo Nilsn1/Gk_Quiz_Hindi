d4de8df03f7248aa2052c745e4e55ca19e5fd66c
admin <nileshsonawanen1@gmail.com>
2023-07-18 23:11:00 +0530

    changes

--- app/src/main/java/com/nilscreation/quizapp/MainActivity.java
+++ app/src/main/java/com/nilscreation/quizapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.nilscreation.quizapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,11 +19,12 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtCurrentQuestion, txtQuestion, optionA, optionB, optionC, optionD, txtNext;
+    TextView txtCurrentQuestion, txtAllQuestionno, txtQuestion, optionA, optionB, optionC, optionD, txtNext;
     ArrayList<QuizModel> datalist;
     int indexNumber = 0;
     int currentQuestionNumber = 1;
     int datalistSize;
+    int correctAnswers = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtCurrentQuestion = findViewById(R.id.txtCurrentQuestion);
+        txtAllQuestionno = findViewById(R.id.txtAllQuestionno);
         txtQuestion = findViewById(R.id.txtQuestion);
         optionA = findViewById(R.id.optionA);
         optionB = findViewById(R.id.optionB);
@@ -43,11 +46,71 @@ public class MainActivity extends AppCompatActivity {
         txtNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                txtNext.setClickable(false);
                 indexNumber = indexNumber + 1;
                 currentQuestionNumber = currentQuestionNumber + 1;
                 setQuestions();
+                enableButtons();
+                Toast.makeText(MainActivity.this, " " + correctAnswers, Toast.LENGTH_SHORT).show();
             }
         });
+
+        optionA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                disableButtons();
+                if (datalist.get(indexNumber).getOptionA().equals(datalist.get(indexNumber).getAnswer())) {
+                    optionA.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.green_card));
+                    correctAnswers = correctAnswers + 1;
+                } else {
+                    optionA.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.red_card));
+                    getRightAnswer();
+                }
+            }
+        });
+
+        optionB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                disableButtons();
+                if (datalist.get(indexNumber).getOptionB().equals(datalist.get(indexNumber).getAnswer())) {
+                    optionB.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.green_card));
+                    correctAnswers = correctAnswers + 1;
+                } else {
+                    optionB.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.red_card));
+                    getRightAnswer();
+                }
+            }
+        });
+
+        optionC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                disableButtons();
+                if (datalist.get(indexNumber).getOptionC().equals(datalist.get(indexNumber).getAnswer())) {
+                    optionC.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.green_card));
+                    correctAnswers = correctAnswers + 1;
+                } else {
+                    optionC.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.red_card));
+                    getRightAnswer();
+                }
+            }
+        });
+
+        optionD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                disableButtons();
+                if (datalist.get(indexNumber).getOptionD().equals(datalist.get(indexNumber).getAnswer())) {
+                    optionD.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.green_card));
+                    correctAnswers = correctAnswers + 1;
+                } else {
+                    optionD.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.red_card));
+                    getRightAnswer();
+                }
+            }
+        });
+        txtNext.setClickable(false);
     }
 
     private void getQuestions() {
@@ -62,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 datalistSize = datalist.size();
                 setQuestions();
+                txtAllQuestionno.setText("" + datalistSize);
             }
 
             @Override
@@ -76,14 +140,46 @@ public class MainActivity extends AppCompatActivity {
 
         if (indexNumber < datalistSize) {
             txtCurrentQuestion.setText("" + currentQuestionNumber);
-            txtQuestion.setText(datalist.get(indexNumber).getQuestion());
-            optionA.setText(datalist.get(indexNumber).getOptionA());
-            optionB.setText(datalist.get(indexNumber).getOptionB());
-            optionC.setText(datalist.get(indexNumber).getOptionC());
-            optionD.setText(datalist.get(indexNumber).getOptionD());
+            txtQuestion.setText("Q. " + datalist.get(indexNumber).getQuestion());
+            optionA.setText("A) " + datalist.get(indexNumber).getOptionA());
+            optionB.setText("B) " + datalist.get(indexNumber).getOptionB());
+            optionC.setText("C) " + datalist.get(indexNumber).getOptionC());
+            optionD.setText("D) " + datalist.get(indexNumber).getOptionD());
         } else {
             Intent intent = new Intent(MainActivity.this, ScoreActivity.class);
+            intent.putExtra("CorrectAnswers", correctAnswers);
             startActivity(intent);
+        }
+    }
+
+    private void enableButtons() {
+        optionA.setClickable(true);
+        optionB.setClickable(true);
+        optionC.setClickable(true);
+        optionD.setClickable(true);
+        optionA.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.white_card));
+        optionB.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.white_card));
+        optionC.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.white_card));
+        optionD.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.white_card));
+    }
+
+    private void disableButtons() {
+        optionA.setClickable(false);
+        optionB.setClickable(false);
+        optionC.setClickable(false);
+        optionD.setClickable(false);
+        txtNext.setClickable(true);
+    }
+
+    private void getRightAnswer() {
+        if (datalist.get(indexNumber).getOptionA().equals(datalist.get(indexNumber).getAnswer())) {
+            optionA.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.green_card));
+        } else if (datalist.get(indexNumber).getOptionB().equals(datalist.get(indexNumber).getAnswer())) {
+            optionB.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.green_card));
+        } else if (datalist.get(indexNumber).getOptionC().equals(datalist.get(indexNumber).getAnswer())) {
+            optionC.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.green_card));
+        } else {
+            optionD.setBackground(ContextCompat.getDrawable(MainActivity.this, R.drawable.green_card));
         }
     }
 }
--- app/src/main/java/com/nilscreation/quizapp/ScoreActivity.java
+++ app/src/main/java/com/nilscreation/quizapp/ScoreActivity.java
@@ -2,13 +2,22 @@ package com.nilscreation.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
+
+    TextView txtCorrectAns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        txtCorrectAns = findViewById(R.id.txtCorrectAns);
+        Intent intent = getIntent();
+        int mCorrectAnswers = intent.getIntExtra("CorrectAnswers", 0);
+        txtCorrectAns.setText("" + mCorrectAnswers);
     }
 }
