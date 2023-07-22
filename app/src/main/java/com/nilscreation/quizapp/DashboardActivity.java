8b35a62ce13000e225bcf0d7daf925ab130d25cc
admin <nileshsonawanen1@gmail.com>
2023-07-22 18:56:05 +0530

    changes

--- app/src/main/java/com/nilscreation/quizapp/DashboardActivity.java
+++ app/src/main/java/com/nilscreation/quizapp/DashboardActivity.java
@@ -2,6 +2,7 @@ package com.nilscreation.quizapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,23 +23,40 @@ public class DashboardActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     ArrayList<String> arrayList;
+    ArrayList<String> filterDatalist;
     CategoryAdapter adapter;
-
     ProgressBar progressbar;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        searchView = findViewById(R.id.searchView);
+        searchView.clearFocus();
         progressbar = findViewById(R.id.progressbar);
 
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         arrayList = new ArrayList<>();
+        filterDatalist = new ArrayList<>();
 
         adapter = new CategoryAdapter(this, arrayList);
         recyclerView.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterData(newText);
+                return true;
+            }
+        });
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Quiz DB");
         databaseReference.addValueEventListener(new ValueEventListener() {
@@ -59,5 +77,17 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void filterData(String query) {
+        filterDatalist.clear();
+        for (String category : arrayList) {
+            if (category.toLowerCase().contains(query.toLowerCase())) {
+                filterDatalist.add(category);
+            }
+        }
+        adapter = new CategoryAdapter(this, filterDatalist);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
